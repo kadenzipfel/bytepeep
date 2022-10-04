bcb7525c4a41d51ad4197898f6e2d0e24fa7b5a3
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-04 14:43:03 -0700

    'non-commutative' => 'commutative'

--- src/rules.rs
+++ src/rules.rs
@@ -2,7 +2,7 @@ use crate::{evm::*, types::*};
 
 pub fn check_rules(peephole: &Bytecode) -> Bytecode {
     let new_bytecode: Bytecode = match peephole[..] {
-        // Redundant swaps on non-commutative operations
+        // Redundant swaps on commutative operations
         [ByteData {opcode: Some(Opcode::Swap1), .. }, ByteData {opcode: Some(Opcode::Add), .. }] => peephole[1..].to_vec(),
         _ => peephole[..].to_vec()
     };
@@ -14,7 +14,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_non_commutative_swaps() {
+    fn test_commutative_swaps() {
         let peephole: Bytecode = vec![
             ByteData { pc: 4, opcode: Some(Opcode::Swap1), pushdata: None, kind: ByteKind::Opcode }, 
             ByteData { pc: 5, opcode: Some(Opcode::Add), pushdata: None, kind: ByteKind::PushData }
