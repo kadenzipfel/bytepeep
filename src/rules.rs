e6b9baa719861027d7cd05cdccb52ed4b4494497
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-06 12:59:30 -0700

    Remaining operations involving an expression and itself

--- src/rules.rs
+++ src/rules.rs
@@ -61,8 +61,166 @@ pub fn check_rules(peephole: &Bytecode) -> Bytecode {
             opcode: Some(Opcode::Or),
             ..
         }] => [].to_vec(),
-        // TODO: Include operations resulting in pushing a value
-            // Requires optimize() accepting returned pushdata
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Xor),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("00")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Sub),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("00")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Eq),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("01")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Lt),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("00")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Slt),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("00")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Gt),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("00")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Sgt),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("00")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
+        [ByteData {
+            opcode: Some(Opcode::Dup1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Mod),
+            ..
+        }] => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(String::from("00")),
+                kind: ByteKind::Opcode
+            }
+        ].to_vec(),
 
         _ => peephole[..].to_vec(),
     };
@@ -244,6 +402,206 @@ mod tests {
             kind: ByteKind::Opcode
         }];
         let optimized_peephole: Vec<ByteData> = Vec::new();
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Xor => Push1, 00
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Xor),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::Opcode
+        }];
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Sub => Push1, 00
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Sub),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::Opcode
+        }];
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Eq => Push1, 01
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Eq),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("01")),
+            kind: ByteKind::Opcode
+        }];
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Lt => Push1, 00
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Lt),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::Opcode
+        }];
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Slt => Push1, 00
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Slt),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::Opcode
+        }];
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Gt => Push1, 00
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Gt),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::Opcode
+        }];
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Sgt => Push1, 00
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Sgt),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::Opcode
+        }];
+        assert_eq!(optimized_peephole, check_rules(&peephole));
+
+        // Dup1, Mod => Push1, 00
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Mod),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::Opcode
+        }];
         assert_eq!(optimized_peephole, check_rules(&peephole));
     }
 }
