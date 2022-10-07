cc9f17244b8141236e9038703f741dd2ac2d046e
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-07 14:11:28 -0700

    Duplicate pushes

--- src/rules.rs
+++ src/rules.rs
@@ -222,6 +222,33 @@ pub fn check_rules(peephole: &Bytecode, pushdata: Vec<Option<PushData>>) -> Byte
             }
         ].to_vec(),
 
+        // Duplicate pushes
+        [ByteData {
+            opcode: Some(Opcode::Push1),
+            ..
+        }, ByteData {
+            opcode: Some(Opcode::Push1),
+            ..
+        }] if pushdata[0].as_ref().unwrap() == pushdata[1].as_ref().unwrap() => [
+            ByteData {
+                pc: peephole[0].pc,
+                opcode: Some(Opcode::Push1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+            ByteData {
+                pc: peephole[1].pc,
+                opcode: None,
+                pushdata: Some(pushdata[0].as_ref().unwrap().to_string()),
+                kind: ByteKind::PushData
+            },
+            ByteData {
+                pc: peephole[1].pc + 1,
+                opcode: Some(Opcode::Dup1),
+                pushdata: None,
+                kind: ByteKind::Opcode
+            },
+        ].to_vec(),
         _ => peephole[..].to_vec(),
     };
     new_bytecode
@@ -618,6 +645,40 @@ mod tests {
             kind: ByteKind::PushData
         }];
         let pushdata: Vec<Option<PushData>> = Vec::new();
+        assert_eq!(optimized_peephole, check_rules(&peephole, pushdata));
+    }
+
+    #[test]
+    fn test_duplicate_pushes() {
+        // Push1, X, Push1, X => Push1, X, Dup1
+        let peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let optimized_peephole = vec![ByteData {
+            pc: 4,
+            opcode: Some(Opcode::Push1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }, ByteData {
+            pc: 5,
+            opcode: None,
+            pushdata: Some(String::from("00")),
+            kind: ByteKind::PushData
+        }, ByteData {
+            pc: 6,
+            opcode: Some(Opcode::Dup1),
+            pushdata: None,
+            kind: ByteKind::Opcode
+        }];
+        let pushdata: Vec<Option<PushData>> = vec![Some(String::from("00")), Some(String::from("00"))];
         assert_eq!(optimized_peephole, check_rules(&peephole, pushdata));
     }
 }
