283742c83e78d4e93ac82ef274c21cb7582031cc
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-07 14:50:58 -0700

    Update ByteData type

--- src/types.rs
+++ src/types.rs
@@ -3,17 +3,10 @@ use crate::evm::Opcode;
 pub type PushData = String;
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum ByteKind {
-    Opcode,
-    PushData,
-}
-
-#[derive(Debug, PartialEq, Clone)]
 pub struct ByteData {
     pub pc: u32,
     pub opcode: Option<Opcode>,
-    pub pushdata: Option<PushData>,
-    pub kind: ByteKind,
+    pub pushdata: Option<PushData>
 }
 
 pub type Bytecode = Vec<ByteData>;
