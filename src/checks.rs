97b5148629607344a7b9f5b25ffdcf8ed75bcfec
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-07 15:22:24 -0700

    Update checks

--- src/checks.rs
+++ src/checks.rs
@@ -4,11 +4,7 @@ use crate::types::*;
 // Check if bytecode contains jumps
 pub fn contains_jumps(bytecode: &Bytecode) -> bool {
     bytecode.iter().any(|byte| {
-        if byte.kind == ByteKind::Opcode {
-            (byte.opcode.unwrap() == Opcode::Jump) || (byte.opcode.unwrap() == Opcode::Jumpi)
-        } else {
-            false
-        }
+        (byte.opcode.unwrap() == Opcode::Jump) || (byte.opcode.unwrap() == Opcode::Jumpi)
     })
 }
 
@@ -20,34 +16,24 @@ mod tests {
     fn test_contains_jumps() {
         let bytecode: Bytecode = vec![
             ByteData {
-                pc: 0,
+                code_index: 0,
                 opcode: Some(Opcode::Push1),
-                pushdata: None,
-                kind: ByteKind::Opcode,
-            },
-            ByteData {
-                pc: 1,
-                opcode: None,
                 pushdata: Some(String::from("4")),
-                kind: ByteKind::PushData,
             },
             ByteData {
-                pc: 2,
+                code_index: 2,
                 opcode: Some(Opcode::Jump),
                 pushdata: None,
-                kind: ByteKind::Opcode,
             },
             ByteData {
-                pc: 3,
+                code_index: 3,
                 opcode: Some(Opcode::Swap1),
                 pushdata: None,
-                kind: ByteKind::Opcode,
             },
             ByteData {
-                pc: 4,
+                code_index: 4,
                 opcode: Some(Opcode::Jumpdest),
                 pushdata: None,
-                kind: ByteKind::Opcode,
             },
         ];
         assert_eq!(true, contains_jumps(&bytecode));
@@ -57,40 +43,24 @@ mod tests {
     fn test_not_contains_jumps() {
         let bytecode: Bytecode = vec![
             ByteData {
-                pc: 0,
+                code_index: 0,
                 opcode: Some(Opcode::Push1),
-                pushdata: None,
-                kind: ByteKind::Opcode,
-            },
-            ByteData {
-                pc: 1,
-                opcode: None,
                 pushdata: Some(String::from("80")),
-                kind: ByteKind::PushData,
             },
             ByteData {
-                pc: 2,
+                code_index: 2,
                 opcode: Some(Opcode::Push1),
-                pushdata: None,
-                kind: ByteKind::Opcode,
-            },
-            ByteData {
-                pc: 3,
-                opcode: None,
                 pushdata: Some(String::from("54")),
-                kind: ByteKind::PushData,
             },
             ByteData {
-                pc: 4,
+                code_index: 4,
                 opcode: Some(Opcode::Swap1),
                 pushdata: None,
-                kind: ByteKind::Opcode,
             },
             ByteData {
-                pc: 5,
+                code_index: 5,
                 opcode: Some(Opcode::Add),
                 pushdata: None,
-                kind: ByteKind::Opcode,
             },
         ];
         assert_eq!(false, contains_jumps(&bytecode));
