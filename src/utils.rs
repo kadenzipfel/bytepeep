dcda837f2d0dd5bbc158e8086c0d216cf3122895
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-07 19:22:24 -0700

    Move match_push_n to utils

--- src/utils.rs
+++ src/utils.rs
@@ -37,3 +37,41 @@ pub fn is_push_op(opcode: Opcode) -> bool {
         _ => false,
     }
 }
+
+pub fn match_push_n(opcode: Opcode) -> usize {
+    match opcode {
+        Opcode::Push1 => 1,
+        Opcode::Push2 => 2,
+        Opcode::Push3 => 3,
+        Opcode::Push4 => 4,
+        Opcode::Push5 => 5,
+        Opcode::Push6 => 6,
+        Opcode::Push7 => 7,
+        Opcode::Push8 => 8,
+        Opcode::Push9 => 9,
+        Opcode::Push10 => 10,
+        Opcode::Push11 => 11,
+        Opcode::Push12 => 12,
+        Opcode::Push13 => 13,
+        Opcode::Push14 => 14,
+        Opcode::Push15 => 15,
+        Opcode::Push16 => 16,
+        Opcode::Push17 => 17,
+        Opcode::Push18 => 18,
+        Opcode::Push19 => 19,
+        Opcode::Push20 => 20,
+        Opcode::Push21 => 21,
+        Opcode::Push22 => 22,
+        Opcode::Push23 => 23,
+        Opcode::Push24 => 24,
+        Opcode::Push25 => 25,
+        Opcode::Push26 => 26,
+        Opcode::Push27 => 27,
+        Opcode::Push28 => 28,
+        Opcode::Push29 => 29,
+        Opcode::Push30 => 30,
+        Opcode::Push31 => 31,
+        Opcode::Push32 => 32,
+        _ => 0,
+    }
+}
