2d96efa1a7a622c78b5100b74e80e57117e96f56
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-08 14:44:07 -0700

    Pretty print diff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use colored::Colorize;
-use std::env;
+use std::{env, iter};
 
 mod assembler;
 mod checks;
@@ -16,16 +16,25 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let bytecode = &args[1];
-    println!("\nBytecode: {}", bytecode);
+    println!("Bytecode: {}", bytecode);
 
     let bytes: Bytecode = disassemble(bytecode);
+    let output_bytes = output(&bytes);
+
     let jump_warning: bool = contains_jumps(&bytes);
+
     let optimized_bytes: Bytecode = optimize(&bytes);
+    let output_optimized_bytes = output(&optimized_bytes);
     let optimized_bytecode = assemble(&optimized_bytes);
 
-    println!("\n\nOptimized bytecode: {}", optimized_bytecode);
+    let left_pad = output_bytes.lines().map(|l| l.len()).max().unwrap_or(0) + 2;
+    println!("\n{:width$} {}", "Unoptimized", "Optimized", width = left_pad);
+    for (output_bytes, output_optimized_bytes) in output_bytes.lines().zip(output_optimized_bytes.lines().chain(iter::repeat(""))) {
+        println!("{:width$} {}", output_bytes, output_optimized_bytes, width = left_pad);
+    }
+
+    println!("\nOptimized bytecode: {}", optimized_bytecode);
     disassemble(&optimized_bytecode);
-    println!("\n");
 
     if jump_warning {
         println!(
